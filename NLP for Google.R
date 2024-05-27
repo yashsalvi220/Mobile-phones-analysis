02fed34dc35a8e6cd3810e46fb6d0ed444d191fa
yashsalvi220 <93437879+yashsalvi220@users.noreply.github.com>
2024-05-27 23:49:22 +0400

    Add files via upload

---
+++ NLP for Google.R
@@ -0,0 +1,152 @@
+#Text Analysis of Google Pixel 5 Phones
+
+#Step 1: Install and load neccessary packages
+install.packages('tm')
+install.packages('SnowballC')
+install.packages('topicmodels')
+install.packages('stopwords')
+install.packages('wordcloud')
+install.packages('Rcpp')
+install.packages('syuzhet')
+library(tm)
+library(SnowballC)
+library(topicmodels)
+library(stopwords)
+library(wordcloud)
+library(Rcpp)
+library(syuzhet)
+#Step2: Load Data
+#Load the Google Pixel 5data
+Pixel5<- read.csv("C:/Users/yashs/OneDrive/Documents/archive (6)/GooglePixel5_reviews.csv")
+#Check dimension of data
+dim(Pixel5)#Shows number of rows and columns
+class(Pixel5)
+#Convert data into a corpus
+mycorpus<- VCorpus(VectorSource(Pixel5$userComments)) #Create a volatile corpus
+writeLines(as.character(mycorpus[[1]]))
+
+#Step3: Preprocess Data
+getTransformations()
+
+#Remove URL from data
+removeURL<- function(x) gsub('http[^[:space:]]*','',x) #Function to remove URLS
+mycorpus<- tm_map(mycorpus,content_transformer(removeURL))
+writeLines(as.character(mycorpus[[124]]))
+
+#Remove Punctuation from data
+mycorpus<-tm_map(mycorpus,removePunctuation)
+writeLines(as.character(mycorpus[[1]]))
+
+#Remove other characters other than English and black spaces
+removeotherchar<- function(x) gsub("[^[:alpha:][:space:]]*","",x)
+mycorpus<-tm_map(mycorpus,content_transformer(removeotherchar))
+writeLines(as.character(mycorpus[[1]]))
+
+#Transform to lower case
+mycorpus<-tm_map(mycorpus,content_transformer(tolower))
+writeLines(as.character(mycorpus[[1]]))
+
+#Remove all English stopwords
+mycorpus<-tm_map(mycorpus,removeWords,c(stopwords('english')))
+writeLines(as.character(mycorpus[[1]]))
+
+#Stemming words to an original form
+mycorpus<-tm_map(mycorpus,stemDocument)
+writeLines(as.character(mycorpus[[1]]))
+
+#Striping whitespace
+mycorpus<-tm.map(mycorpus,stripWhitespace)
+writeLines(as.character(mycorpus[[1]]))
+
+#Topic modelling
+#Step4: Create document-term matrix. This helps in classifying rows for analysis
+#Weighting system1: term frequency with focus on word letter
+mat<-DocumentTermMatrix(mycorpus,control = list(wordLengths=c(3,20)))
+mat
+
+# Step5: Build Topic Model
+model1<- LDA(mat,k=7) #select five topics for our analysis
+ldout.topics<-as.matrix(topics(model1))
+write.csv(ldout.topics,"topic_label.csv",row.names= TRUE)
+
+#Build key words related to each topic
+ldaout.terms<-(terms<-terms(model1,10))
+write.csv(ldaout.terms,"terms.csv",row.names = FALSE)
+
+#Converting data into term document matrix
+TextDoc<-TermDocumentMatrix(mycorpus)
+tdm<-as.matrix(tdm)
+tdm[1:10,1:20]
+#Building bar plot
+w<-rowSums(tdm)
+w<-subset(w,w>25)
+barplot(w,las=2,col = rainbow(50))
+
+#Build plot of most frequently used items
+w<-sort(rowSums(tdm), decreasing = TRUE)
+wordfrequency<- data.frame(word=names(w),freq= w)
+head(wordfrequency,5)
+# Plot the most frequent words
+barplot(wordfrequency[1:5,]$freq, las = 2, names.arg = wordfrequency[1:5,]$word,
+        col ="lightgreen", main ="Top 5 most frequent words",
+        ylab = "Word frequencies")
+
+#Build wordcloud for analysis
+library(wordcloud)
+w <- sort(rowSums(tdm), decreasing = TRUE)
+set.seed(222)
+wordcloud(words = names(w),
+          freq = w,
+          max.words = 150,
+          random.order = F,
+          min.freq = 5,
+          colors = brewer.pal(8, 'Dark2'),
+          scale = c(5, 0.3),
+          rot.per = 0.7)
+
+#Find association among words
+findAssocs(TextDoc, terms= c("like","batteri","camera"), corlimit= 0.25)
+#Find association among words occuring 50 times a week
+findAssocs(TextDoc, terms = findFreqTerms(TextDoc,lowfreq = 50), corlimit= 0.25) 
+
+#Sentiment analysis of words
+Pixel5<- read.csv("C:/Users/13345/Dropbox/My PC (DESKTOP-B1D9GNS)/Downloads/archive (6)/GooglePixel5_reviews.csv")
+sentiment<- iconv(Pixel5$userComments)
+s <- get_nrc_sentiment(sentiment)
+head(s)
+
+#Build barplot to understand relationship between posts
+barplot(colSums(s),
+        las = 2,
+        col = rainbow(10),
+        ylab = 'Count',
+        main = 'Sentiment Scores Tweets')
+
+#Comparison of sentiment with other measures
+#syuzhet method
+syuzhet_vector <- get_sentiment(sentiment, method="syuzhet")
+head(syuzhet_vector)
+summary(syuzhet_vector)
+# bing method
+bing_vector <- get_sentiment(sentiment, method="bing")
+head(bing_vector)
+summary(bing_vector)
+#affin method
+afinn_vector <- get_sentiment(sentiment, method="afinn")
+head(afinn_vector)
+summary(afinn_vector)
+
+#compare the first row of each vector using sign function
+rbind(
+  sign(head(syuzhet_vector)),
+  sign(head(bing_vector)),
+  sign(head(afinn_vector)))
+
+#Emotions percentage
+barplot(
+  sort(colSums(prop.table(s[, 1:8]))), 
+  horiz = TRUE, 
+  cex.names = 0.7, 
+  las = 1, 
+  main = "Emotions in Text", xlab="Percentage"
+)
---
+++ Samsung.R
@@ -0,0 +1,144 @@
+library(tm)
+library(SnowballC)
+library(topicmodels)
+library(stopwords)
+library(wordcloud)
+library(Rcpp)
+library(syuzhet)
+library(readr)
+#Step2: Load Data
+#Load the Google Pixel 5data
+Samsung<- read.csv("C:/Users/yashs/OneDrive/Documents/archive (6)/SamsungGalaxyS21Ultra_reviews.csv") 
+
+#Check dimension of data
+dim(Samsung)#Shows number of rows and columns
+class(Samsung)
+#Convert data into a corpus
+mycorpus<- VCorpus(VectorSource(Samsung$userComments)) #Create a volatile corpus
+writeLines(as.character(mycorpus[[1]]))
+
+#Step3: Preprocess Data
+getTransformations()
+
+#Remove URL from data
+removeURL<- function(x) gsub('http[^[:space:]]*','',x) #Function to remove URLS
+mycorpus<- tm_map(mycorpus,content_transformer(removeURL))
+writeLines(as.character(mycorpus[[2]]))
+
+#Remove Punctuation from data
+mycorpus<-tm_map(mycorpus,removePunctuation)
+writeLines(as.character(mycorpus[[2]]))
+
+#Remove other characters other than English and black spaces
+removeotherchar<- function(x) gsub("[^[:alpha:][:space:]]*","",x)
+mycorpus<-tm_map(mycorpus,content_transformer(removeotherchar))
+writeLines(as.character(mycorpus[[2]]))
+
+#Transform to lower case
+mycorpus<-tm_map(mycorpus,content_transformer(tolower))
+writeLines(as.character(mycorpus[[2]]))
+
+#Remove all English stopwords
+mycorpus<-tm_map(mycorpus,removeWords,c(stopwords('english')))
+writeLines(as.character(mycorpus[[2]]))
+
+#Stemming words to an original form
+mycorpus<-tm_map(mycorpus,stemDocument)
+writeLines(as.character(mycorpus[[2]]))
+
+#Striping whitespace
+mycorpus<-tm_map(mycorpus,stripWhitespace)
+writeLines(as.character(mycorpus[[2]]))
+
+#Topic modelling
+#Step4: Create document-term matrix. This helps in classifying rows for analysis
+#Weighting system1: term frequency with focus on word letter
+mat<-DocumentTermMatrix(mycorpus,control = list(wordLengths=c(3,20)))
+mat
+raw.sum=apply(mat,1,FUN=sum)
+mat=mat[raw.sum!=0,]
+# Step5: Build Topic Model
+model1<- LDA(mat,k=5) #select five topics for our analysis
+ldout.topics<-as.matrix(topics(model1))
+write.csv(ldout.topics,"topic_label_Samsung.csv",row.names= TRUE)
+
+#Build key words related to each topic
+ldaout.terms<-(terms<-terms(model1,10))
+write.csv(ldaout.terms,"terms_Samsung.csv",row.names = FALSE)
+
+#Converting data into term document matrix
+TextDoc<-TermDocumentMatrix(mycorpus)
+tdm<-as.matrix(TextDoc)
+tdm[1:10,1:20]
+#Building bar plot
+w<-rowSums(tdm)
+w<-subset(w,w>25)
+barplot(w,las=2,col = rainbow(50))
+
+#Build plot of most frequently used items
+w<-sort(rowSums(tdm), decreasing = TRUE)
+wordfrequency<- data.frame(word=names(w),freq= w)
+head(wordfrequency,10)
+# Plot the most frequent words
+barplot(wordfrequency[1:5,]$freq, las = 2, names.arg = wordfrequency[1:5,]$word,
+        col ="lightgreen", main ="Top 5 most frequent words",
+        ylab = "Word frequencies")
+
+#Build wordcloud for analysis
+library(wordcloud)
+w <- sort(rowSums(tdm), decreasing = TRUE)
+set.seed(222)
+wordcloud(words = names(w),
+          freq = w,
+          max.words = 150,
+          random.order = F,
+          min.freq = 5,
+          colors = brewer.pal(8, 'Dark2'),
+          scale = c(5, 0.3),
+          rot.per = 0.7)
+
+#Find association among words
+findAssocs(TextDoc, terms= c("like","batteri","camera"), corlimit= 0.25)
+#Find association among words occuring 50 times a week
+findAssocs(TextDoc, terms = findFreqTerms(TextDoc,lowfreq = 50), corlimit= 0.25) 
+
+#Sentiment analysis of words
+Samsung<- read.csv("C:/Users/13345/Dropbox/My PC (DESKTOP-B1D9GNS)/Downloads/archive (6)/SamsungGalaxyS21Ultra_reviews.csv")
+sentiment<- iconv(Samsung$userComments)
+s <- get_nrc_sentiment(sentiment)
+head(s)
+
+#Build barplot to understand relationship between posts
+barplot(colSums(s),
+        las = 2,
+        col = rainbow(10),
+        ylab = 'Count',
+        main = 'Sentiment Scores Tweets')
+
+#Comparison of sentiment with other measures
+#syuzhet method
+syuzhet_vector <- get_sentiment(sentiment, method="syuzhet")
+head(syuzhet_vector)
+summary(syuzhet_vector)
+# bing method
+bing_vector <- get_sentiment(sentiment, method="bing")
+head(bing_vector)
+summary(bing_vector)
+#affin method
+afinn_vector <- get_sentiment(sentiment, method="afinn")
+head(afinn_vector)
+summary(afinn_vector)
+
+#compare the first row of each vector using sign function
+rbind(
+  sign(head(syuzhet_vector)),
+  sign(head(bing_vector)),
+  sign(head(afinn_vector)))
+
+#Emotions percentage
+barplot(
+  sort(colSums(prop.table(s[, 1:8]))), 
+  horiz = TRUE, 
+  cex.names = 0.7, 
+  las = 1, 
+  main = "Emotions in Text", xlab="Percentage")
